46f1e5de50e7d505ef29e0408ebf2aa0334a8f77
Tayna-Macedo <tayna_sm1996@hotmail.com>
2022-09-23 19:25:22 -0300

    feat: Requisito 7: Criação da model Category

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      tableName: 'categories',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  return Category;
+};
